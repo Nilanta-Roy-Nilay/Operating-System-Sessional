a02a4291c95e2ab17e8f283e894a9201cb8c6c5c
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-08-28 19:07:11 +0000

    Update fcfs.sh

--- FCFS.sh/fcfs.sh
+++ FCFS.sh/fcfs.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+echo "Enter number of processes:"
+read n;
+
+bt=()
+for((i=0; i<n;i++))
+do
+  echo "Enter burst time for P$((i+1)):"
+  read bt[$i]
+done
+
+wt[0]=0
+tat[0]=${bt[0]}
+
+for ((i=1; i<n; i++))
+do 
+  wt[$i]=$((wt[i-1] + bt[i-1]))
+  tat[$i]=$((wt[i] + bt[i]))
+done
+
+echo -e "\nProcess\tBurst\tWaiting\tTurnaround"
+for ((i=0; i<n; i++))
+do 
+  echo "P$((i+1))  ${bt[i]} ${wt[i]} ${tat[i]}"
+done
