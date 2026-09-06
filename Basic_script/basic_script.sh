7bef38f329bcf2bf6e237aff8596854f21638ff8
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-09-06 12:23:23 +0600

    Add files via upload

--- Basic_script/basic_script.sh
+++ Basic_script/basic_script.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-fruits=("Apple" "Banana" "Mango")
-fruits=("${fruits[@]:0:2}" "Grapes" "${fruits[@]:2}")
+name="NILAY"
+echo "Hello,$name!" 
+
+current_dir=$(pwd)
+echo "You are in: $current_dir"
+
+read -p "Enter your age: " age
+echo "You are $age years old."
+
+age=25
+printf "name: %s\nage: %d\n""$name""$age"
+
+fruits=("Apple" "Banana" "Orange")
 colors=()
 colors[0]="Red"
 colors[1]="Green"
 colors[2]="Blue"
 
-echo ${fruits[0]} #apple
-echo ${fruits[1]} #apple
-echo ${fruits[@]} #apple
-echo ${#fruits[0]} #apple
+echo ${fruits[0]}
+echo ${fruits[1]}
+echo ${fruits[@]}
+echo ${#fruits[@]}
 
-fruits2=("Lemon""Orange")
-fruits2+=("blackberry")
-echo ${#fruits2[@]}
+fruits=("Apple" "Banana")
+fruits+=("Orange")
+echo ${fruits[@]}
 
-do 
-  echo "I like $fruitttt"
+unset fruits[1]
+echo ${fruits[@]}
+
+for i in {1,5}
+do
+  echo "Number:$i"
 done
-
-
